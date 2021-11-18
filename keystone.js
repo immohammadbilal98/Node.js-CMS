00982a6de7c174dc0b783616a0dc8665fe5b06c1
immohammedbilal98 <im.mohammedbilal98@gmail.com>
2021-11-18 19:40:09 +0400

    make it better

--- keystone.js
+++ keystone.js
@@ -63,7 +63,7 @@ keystone.set("nav", {
 
 keystone.start();
 
-CLOUD SERVER DATABASE
+//CLOUD SERVER DATABASE
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
