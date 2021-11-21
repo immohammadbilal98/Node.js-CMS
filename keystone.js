80d304f73959802c2c13bf0a329837734f1295fe
immohammedbilal98 <im.mohammedbilal98@gmail.com>
2021-11-21 15:55:49 +0400

    make it better

--- keystone.js
+++ keystone.js
@@ -20,9 +20,8 @@ keystone.init({
 	views: "templates/views",
 	"view engine": ".hbs",
 	"mongo": process.env.MONGO_URI,
-	"cloudinary config": process.env.CLOUDINARY_URL, //CLOUDINARY.
+	"cloudinary config": process.env.CLOUDINARY_URL, //CLOUDINARY.....
 	"cookie secret": process.env.COOKIE_SECRET,
-
 	"custom engine": handlebars.create({
 		layoutsDir: "templates/views/layouts",
 		partialsDir: "templates/views/partials",
